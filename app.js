33094b63f4b230b7900ae91659394050699d21dd
Jakub Mitkowski <64985685+Miitecc@users.noreply.github.com>
2022-10-15 20:58:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const champions = ["aatrox","ahri","akali","akshan","alistar","amumu","anivia","annie","aphelios","ashe","aurelion sol","azir","bard","bel'Veth","blitzcrank","brand","braum","caitlyn","camille","cassiopeia","cho'gath","corki","darius","diana","dr mundo","draven","ekko","elise","evelynn","ezreal","fiddlesticks","fiora","fizz","galio","gangplank","garen","gnar","gragas","graves","gwen","hecarim","heimerdinger","illaoi","irelia","ivern","janna","jarvan IV","jax","jayce","jhin","jinx","kai'sa","kalista","karma","karthus","kassadin","katarina","kayle","kayn","kennen","kha'zix","kindred","kled","kog'maw","leblanc","lee sin","leona","lillia","lissandra","lucian","lulu","lux","malphite","malzahar","maokai","master yi","miss fortune","mordekaiser","morgana","nami","nasus","nautilus","neeko","nidalee","nilah","nocturne","nunu & willump","olaf","orianna","ornn","pantheon","poppy","pyke","qiyana","quinn","rakan","rammus","rek'Sai","rell","renata glasc","renekton","rengar","riven","rumble","ryze","samira","sejuani","senna","seraphine","sett","shaco","shen","shyvana","singed","sion","sivir","skarner","sona","soraka","swain","sylas","syndra","tahm kench","taliyah","talon","taric","teemo","thresh","tristana","trundle","tryndamere","twisted fate","twitch","udyr","urgot","varus","vayne","veigar","vel'koz","vex","vi","viego","viktor","vladimir","volibear","warwick","wukong","xayah","xerath","xin zhao","yasuo","yone","yorick","yuumi","zac","zed","zeri","ziggs","zilean","zoe","zyra"];
+let score = 0;
+
+setInterval(checkScore, 1000);
+
+
+$("#champion-name").keypress(function(event) {
+    if (event.keyCode === 13) {
+        $("#btn").click();
+    }
+});
+
+$("#btn").click(function() {
+    const postac = document.getElementById('champion-name').value;
+
+    for(var i=0;i<=161;i++){
+       if(postac==champions[i] && champions[i]!=="finded") {
+        let licznik = i+1;
+        $('.app-window>div:nth-child('+ licznik +')').addClass('solved').removeClass('champ-cover');
+        $('.app-window>div:nth-child('+ licznik +')').addClass('new'+licznik)
+        score = score + 1;
+        document.getElementById("actual-score").innerHTML = score;
+        champions.splice(i,1,"finded");
+
+         $('body, html').animate({
+            scrollTop: $(".new"+licznik).offset().top
+        },500)
+       }
+       else{
+       }
+    }
+    $('#champion-name').val('');
+});
+
+function checkScore() {
+    if(score===161) {
+        for (let i = 0; i <= 161; i++) {
+            if (champions[i] === "finded") {
+                let licznik = i + 1;
+                $('.app-window>div:nth-child(' + licznik + ')').addClass('success').removeClass('solved');
+                champions.splice(i, 1, "success");
+                $('.input-bar').addClass('hidden');
+                $('.bgc').addClass('hidden');
+                $('#timer').addClass('hidden');
+            }
+            else { }
+    
+        }
+    }
+}
+
+
+window.onbeforeunload = function() {
+    return "Data will be lost if you leave the page, are you sure?";
+  };
+
+
+
+
+
+
+
+
+
+
